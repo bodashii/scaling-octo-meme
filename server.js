f2ddf99d78f2be4d385e9cfc243941db01a153c2
maddnty <nzkb24@gmail.com>
2022-06-11 10:09:17 -0500

    established server connection

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const { db } = require('./db/db.json');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
